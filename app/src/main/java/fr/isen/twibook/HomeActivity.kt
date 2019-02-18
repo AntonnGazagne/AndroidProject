d2e5c94b471bcec920aa6eadf772ea81c137742c
Antonn Gazagne <antonntln83@gmail.com>
2019-02-18 15:13:55 +0100

    Initial Project

--- app/src/main/java/fr/isen/twibook/HomeActivity.kt
+++ app/src/main/java/fr/isen/twibook/HomeActivity.kt
@@ -1,7 +1,6 @@
 package fr.isen.twibook
 
 import android.os.Bundle
-import android.support.design.widget.Snackbar
 import android.support.v7.app.AppCompatActivity;
 
 import kotlinx.android.synthetic.main.activity_home.*
@@ -11,12 +10,7 @@ class HomeActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_home)
-        setSupportActionBar(toolbar)
 
-        fab.setOnClickListener { view ->
-            Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                .setAction("Action", null).show()
-        }
     }
 
 }
